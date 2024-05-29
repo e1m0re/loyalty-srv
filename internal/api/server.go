e0323bb2858747c09bc0aa92a29099e446eb91f5
vlogashenko <vl@phishman.ru>
2024-05-29 23:36:02 +0300

    add migration

--- internal/api/server.go
+++ internal/api/server.go
@@ -2,8 +2,11 @@ package api
 
 import (
 	"context"
+	"e1m0re/loyalty-srv/internal/db/migrations"
 	"errors"
 	"fmt"
+	"github.com/jackc/pgx/v5/stdlib"
+	"github.com/pressly/goose/v3"
 	"log/slog"
 	"net/http"
 
@@ -42,6 +45,28 @@ func NewServer(ctx context.Context, cfg *Config) (*Server, error) {
 	return srv, nil
 }
 
+func (srv *Server) prepareDB(ctx context.Context) error {
+	stdlib.GetDefaultDriver()
+
+	db, err := goose.OpenDBWithDriver("pgx", srv.config.databaseDSN)
+	if err != nil {
+		return err
+	}
+
+	goose.SetBaseFS(&migrations.Content)
+	err = goose.SetDialect("postgres")
+	if err != nil {
+		return err
+	}
+
+	err = goose.Up(db, ".")
+	if err != nil {
+		return err
+	}
+
+	return db.Close()
+}
+
 func (srv *Server) startHTTPServer(ctx context.Context) error {
 	slog.Info(fmt.Sprintf("starting http server at %s", srv.httpServer.Addr))
 	err := srv.httpServer.ListenAndServe()
@@ -53,6 +78,11 @@ func (srv *Server) startHTTPServer(ctx context.Context) error {
 }
 
 func (srv *Server) Start(ctx context.Context) error {
+	err := srv.prepareDB(ctx)
+	if err != nil {
+		return err
+	}
+
 	grp, ctx := errgroup.WithContext(ctx)
 
 	grp.Go(func() error {
--- internal/db/migrations/20240511084504_init.sql
+++ internal/db/migrations/20240511084504_init.sql
@@ -1,6 +1,54 @@
 -- +goose Up
 -- +goose StatementBegin
 SELECT 'up SQL query';
+CREATE TABLE users
+(
+    id         SERIAL
+        CONSTRAINT users_pk PRIMARY KEY,
+    password   VARCHAR(255) NOT NULL,
+    last_login TIMESTAMP,
+    username   VARCHAR(50)  NOT NULL UNIQUE
+);
+
+CREATE TABLE orders
+(
+    id         SERIAL
+        CONSTRAINT orders_pk
+            PRIMARY KEY,
+    "user"     INTEGER
+        CONSTRAINT orders_users_id_fk
+            REFERENCES users,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    status     VARCHAR(50)                         NOT NULL,
+    number     VARCHAR(50)
+        UNIQUE,
+    accrual    INTEGER,
+    calculated BOOLEAN   DEFAULT false             NOT NULL
+);
+
+CREATE TABLE accounts
+(
+    id      SERIAL
+        CONSTRAINT accounts_pk
+            PRIMARY KEY,
+    "user"  INTEGER NOT NULL
+        CONSTRAINT accounts_users_id_fk
+            REFERENCES users,
+    balance money   NOT NULL
+);
+
+CREATE TABLE accounts_changes
+(
+    id      SERIAL
+        CONSTRAINT accounts_changes_pk
+            PRIMARY KEY,
+    account INTEGER   NOT NULL
+        CONSTRAINT accounts_changes_accounts_id_fk
+            REFERENCES accounts,
+    amount  money     NOT NULL,
+    ts      TIMESTAMP NOT NULL,
+    "order" VARCHAR(50)
+);
 -- +goose StatementEnd
 
 -- +goose Down
